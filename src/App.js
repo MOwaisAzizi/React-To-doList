9860e544eede31a3b7ca7b782e7bb9be3fb48ef3
owias azizi <owaisazizi@jmail.com>
2025-01-01 16:50:44 +0430

    add update functins

--- src/App.js
+++ src/App.js
@@ -7,7 +7,34 @@ function App() {
   const [inputValue, setInputValue] = useState('');
   const [editIndex, setEditIndex] = useState(null);
 
-
+  const handleInputChange = (e) => {
+      setInputValue(e.target.value);
+   };
+  
+    const handleAddTodo = () => {
+      if (inputValue.trim()) {
+        if (editIndex !== null) {
+          const updatedTodos = todos.map((todo, index) =>
+            index === editIndex ? inputValue : todo
+          );
+          setTodos(updatedTodos);
+          setEditIndex(null);
+        } else {
+          setTodos([...todos, inputValue]);
+        }
+        setInputValue('');
+      }
+    };
+  
+    const handleEditTodo = (index) => {
+      setInputValue(todos[index]);
+      setEditIndex(index);
+    };
+  
+    const handleDeleteTodo = (index) => {
+      const updatedTodos = todos.filter((_, i) => i !== index);
+      setTodos(updatedTodos);
+    };
 
   return (
     <div className="App">
